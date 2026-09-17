8969ab8adc5db802c484171724a92b0efe524031
DaRae Park <ret7258@naver.com>
2026-09-17 22:21:53 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/darae/tourweather/TourWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.darae.tourweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darae/tourweather/TourWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darae.tourweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
